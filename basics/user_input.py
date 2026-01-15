86f4e684173979c101cc023d148f5c494a4b5d76
SwathiVanka07 <vankaswathi7@gmail.com>
2026-01-15 21:41:14 +0530

    Implement user input for name and age
    Added user input functionality to capture name and age.

---
+++ basics/user_input.py
@@ -0,0 +1,3 @@
+name = input("Enter name:" )              # input() is used to get user input
+age = int(input("Enter age:" )             # int() is used to  convert the input the user provides into an integer
+print(f" My name is {name} and I am {age} years old.")
